6596b400039941335e4a43482efebaa5c741a0bd
quxaing <3076885334@qq.com>
2018-10-18 17:39:01 +0800

    'init'

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+//注册页面
+router.get('/register',function(req,res){
+  res.render('register');
+})
+
+module.exports = router;
