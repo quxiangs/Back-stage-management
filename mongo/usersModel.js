a0b9b75d58b0fba707d81f8c33e68f74110a5107
quxaing <3076885334@qq.com>
2018-10-19 01:21:49 +0800

    '二次修改'

---
+++ mongo/usersModel.js
@@ -0,0 +1,53 @@
+//该模块用来 操作users 相关的后台数据库处理代码
+//注册操作
+//登录操作
+//修改操作
+//删除操作
+//查询列表操作
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://127.0.0.1:27017';
+
+const usersModel = {
+    /**
+   *     注册操作
+   *     @param {Object} data 注册信息
+   *    @param {Function} cb 回调函数
+   */
+    add(data,cb){
+        MongoClient.connect(url,function(err,client){
+            if(err) throw err;
+            const db = client.db('zwpan');
+            //1.对data里面的isAdmin修改为is_admin
+            //2.写一个id为1
+            //下一个注册要得到之前用户表的记录条数加1之后写给下一个注册的人
+            //不允许用户名相同。
+            let saveData= {
+                username:data.username,
+                password: data.password,
+                nickname: data.nickname,
+                phone: data.phone,
+                is_admin: data.isAdmin
+            };
+            db.collection('users').find({ username: saveData.username}).count(function(err,num){
+                // 如果 num 为 0 ，没有注册，否则已经注册了
+                if(err) throw err;
+                if(num===0){
+                    db.collection('users').find().count(function(err,num){
+                        if(err) throw err;
+                        saveData._id = num+1;
+                        db.collection('users').insertOne(saveData,function(err){
+                            if(err) throw err;
+                            cb(null);
+                            client.close();
+                        });
+                    });
+                }else{
+                    console.log("00000000")
+                    cd('已经注册过了');
+                    client.close();
+                }
+            })
+        })
+    }
+}
+module.exports = usersModel;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
-
+const usersModel = require('../mongo/usersModel');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/register',function (req,res) {
+  // console.log(req.body); 
+  //1.用户名必须5-10位 [A-Za-z0-9_]
+  if (!/^\w{5,10}$/.test(req.body.username)){
+    // res.send('请输入正确的用户名,用户名必须是5到10位');
+    res.render('verror',{code:-1,msg:'请输入正确的用户名,用户名必须是5到10位'});
+  }
+  //2.密码验证 /^[a-z0-9_]{6,10}$/
+  if (!/^[A-Za-z0-9_]{6,18}$/.test(req.body.password)){
+    res.render('verror',{code:0,msg:'密码为6到10位,由字母数字下划线组成'});
+  }
+  //3.密码一致验证
+  if( req.body.password != req.body.repassword ){
+    res.render('verror', { code: 1, msg: '密码必须一致' });
+  }
+  //4.nickname验证 /^[a-zA-Z0-9_\u4e00-\u9fa5]{2,8}$/
+  if (!/^[a-zA-Z0-9_\u4e00-\u9fa5]{2,8}$/.test(req.body.nickname)){
+    res.render('verror', { code: 2, msg: '请输入正确昵称2到8位' });
+  }
+  //5.手机号验证 /^1[34578]\d{9}$/
+  if (!/^1[34578]\d{9}$/.test(req.body.phone)){
+    res.render('verror',{code:-2,msg:'请输入正确的手机号'});
+  }
+//操作数据库写入信息
+try{
+  usersModel.add(req.body,function (err) {
+    if(err) throw err;
+    console.log("成功添加")
+    res.render('login');
+  })
+}catch(error){
+  console.log('失败' + error);
+  res.render('verror', { code: 3, msg:'error'})
+}
+});
 module.exports = router;
